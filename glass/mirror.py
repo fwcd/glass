25c5033cb7dac16b7c9f515ceaa4e574624e1e1a
fwcd <fwcdmail@gmail.com>
2021-01-20 03:09:19 +0100

    Update mirror function

--- glass/mirror.py
+++ glass/mirror.py
@@ -1,10 +1,13 @@
+import os
 import subprocess
 
 def mirror_repo(repo_url, target_dir):
     dir_name = repo_url.rsplit('/', 1)[-1]
-    code = subprocess.run(['git', 'clone', repo_url, '--mirror'], cwd=target_dir).returncode
-    if code == 0:
-        print(f'Mirrored {repo_url}')
+    repo_dir = f'{target_dir}/{dir_name}'
+
+    if os.path.exists(repo_dir):
+        print(f'Updating {repo_url}...')
+        subprocess.run(['git', 'remote', 'update'], cwd=repo_dir)
     else:
-        subprocess.run(['git', 'remote', 'update'], cwd=f'{target_dir}/{dir_name}')
-        print(f'Updated {repo_url}')
+        print(f'Mirroring {repo_url}...')
+        subprocess.run(['git', 'clone', repo_url, '--mirror'], cwd=target_dir)
