1b5e6d71af903b6c12b207da9b861a477d84df81
fwcd <fwcdmail@gmail.com>
2021-01-27 02:07:13 +0100

    Add default config path

--- glass/__init__.py
+++ glass/__init__.py
@@ -1,11 +1,13 @@
 import argparse
 import json
 import os
+from pathlib import Path
 
 from glass.hoster.git import SingleRepoGitHoster
 from glass.hoster.github import GitHubHoster
 from glass.mirror import mirror_repo
 
+DEFAULT_CONFIG_PATH = Path.home() / '.config' / 'glass' / 'config.json'
 HOSTERS = {
     'github': lambda acc: GitHubHoster(acc['username'], acc['token']),
     'git': lambda _: SingleRepoGitHoster()
@@ -13,7 +15,7 @@ HOSTERS = {
 
 def main():
     parser = argparse.ArgumentParser(description='Tool for mirroring GitHub repositories.')
-    parser.add_argument('--config', required=True, help='Path to your config.json')
+    parser.add_argument('--config', required=not DEFAULT_CONFIG_PATH.exists(), default=str(DEFAULT_CONFIG_PATH), help='Path to your config.json')
 
     args = parser.parse_args()
     with open(args.config, 'r') as f:
@@ -28,8 +30,10 @@ def main():
         hoster = HOSTERS[acc_type](account)
 
         if hoster:
+            print(f"Querying {acc_desc}...")
             repo_urls = hoster.repositories()
             print(f"Found {len(repo_urls)} repo(s) on {acc_desc}...")
+
             for url in repo_urls:
                 mirror_repo(url, os.path.join(target_dir, acc_desc))
         else:
