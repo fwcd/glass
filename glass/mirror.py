9ca93df8fc229a32215d50d0dffb7efd54040866
fwcd <fwcdmail@gmail.com>
2021-01-27 02:43:15 +0100

    Improve mirroring logic and dir structure example

--- glass/mirror.py
+++ glass/mirror.py
@@ -1,15 +1,14 @@
 import subprocess
+from pathlib import Path
+from urllib.parse import urlparse
 
 def mirror_repo(repo_url, target_dir):
     target_dir.mkdir(parents=True, exist_ok=True)
-
-    # TODO: Mirror repo to path under username/group/subgroup dir
-    dir_name = repo_url.rsplit('/', 1)[-1]
-    repo_dir = target_dir / dir_name
+    repo_dir = Path(str(target_dir) + urlparse(repo_url).path)
 
     if repo_dir.exists():
         print(f'Updating from {repo_url}...')
-        subprocess.run(['git', 'remote', 'update'], cwd=repo_dir)
+        subprocess.run(['git', 'remote', 'update'], cwd=str(repo_dir))
     else:
         print(f'Mirroring from {repo_url}...')
-        subprocess.run(['git', 'clone', repo_url, '--mirror'], cwd=str(target_dir))
+        subprocess.run(['git', 'clone', repo_url, '--mirror'], cwd=str(repo_dir.parent))
